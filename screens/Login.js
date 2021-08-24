2f618d748bd47beae78299da8600dcbc83cd23fc
pavlo-osypov-by <pavlo.osypov@edvantis.com>
2021-08-24 09:18:40 +0300

    Login screen

--- screens/Login.js
+++ screens/Login.js
@@ -2,24 +2,134 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity
+    TouchableOpacity,
+    ImageBackground,
+    StatusBar
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient'
+
+import { images, COLORS, SIZES, FONTS } from '../constants'
+import { CustomButton } from '../components/index'
 
 const Login = ({ navigation }) => {
+
+    function renderHeader() {
+        return (
+            <View
+                style={{
+                    height: SIZES.height > 700 ? '65%' : '60%'
+                }}
+            >
+                <ImageBackground
+                    source={images.loginBackground}
+                    style={{
+                        flex: 1,
+                        justifyContent: 'flex-end'
+                    }}
+                    resizeMode='cover'
+                >
+                    <LinearGradient
+                        start={{ x: 0, y: 0 }}
+                        end={{ x: 0, y: 1 }}
+                        colors={[
+                            COLORS.transparent,
+                            COLORS.black
+                        ]}
+                        style={{
+                            height: 200,
+                            justifyContent: 'flex-end',
+                            paddingHorizontal: SIZES.padding
+                        }}
+                    >
+                        <Text
+                            style={{
+                                width: '80%',
+                                color: COLORS.white,
+                                ...FONTS.largeTitle,
+                                lineHeight: 45
+                            }}>
+                            Cooking a Delicious Food Easily
+                        </Text>
+                    </LinearGradient>
+                </ImageBackground>
+            </View>
+        )
+    }
+
+    function renderDetail() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    paddingHorizontal: SIZES.padding
+                }}
+            >
+                {/* Description  */}
+                <Text
+                    style={{
+                        marginTop: SIZES.radius,
+                        width: '70%',
+                        color: COLORS.gray,
+                        ...FONTS.body3
+                    }}
+                >
+                    Discover more than 1200 food recipies in your hands and cooking it easily!
+                </Text>
+
+                {/* Buttons */}
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'flex-end'
+                    }}
+                >
+                    {/* Login  */}
+                    <CustomButton
+                        buttonText='Login'
+                        buttonContainerStyle={{
+                            paddingVertical: 18,
+                            borderRadius: 20
+                        }}
+                        colors={[COLORS.darkGreen, COLORS.lime]}
+                        onPress={() => navigation.replace('Home')}
+                    />
+
+                    {/* Sign Up */}
+                    <CustomButton
+                        buttonText='Sign UP'
+                        buttonContainerStyle={{
+                            marginTop: SIZES.radius,
+                            paddingVertical: 18,
+                            borderRadius: 20,
+                            borderColor: COLORS.darkLime,
+                            borderWidth: 1
+                        }}
+                        colors={[]}
+                        onPress={() => navigation.replace('Home')}
+                    />
+
+                </View>
+
+
+            </View >
+        )
+    }
+
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                backgroundColor: COLORS.black
             }}
         >
-            <Text>Login</Text>
-            <TouchableOpacity
-                onPress={() => navigation.replace("Home")}
-            >
-                <Text>Navigate to Home</Text>
-            </TouchableOpacity>
+            <StatusBar barStyle='light-content' />
+
+            {/* Header  */}
+            {renderHeader()}
+
+            {/* Details  */}
+            {renderDetail()}
+
         </View>
     )
 }
